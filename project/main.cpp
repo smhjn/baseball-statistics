85eba2525a047099512e1d7e8df776d521d78493
ProMichael <ProMichael@outlook.com>
2014-12-23 10:07:06 -0500

    Create main.cpp

---
+++ project/main.cpp
@@ -0,0 +1,19 @@
+/*
+*   Authors: Michael Golebiewski
+*            Victor Jaquez
+*            Kevin Hrebenko
+*   Desc; Main file for the baseball stats program
+*   Date: 12/23/14
+*/
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    bool Loop = true;
+    if(Loop == true) {
+        cout << "Welcome to Baseball Stat"
+    } else {
+        return 0;
+    }
+}
