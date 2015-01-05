263b241f1ddc842a447e7f55dd2ecfa8694e23ed
ProMichael <ProMichael@outlook.com>
2015-01-05 04:20:07 -0800

    Create hitting.h

---
+++ project/hitting.h
@@ -0,0 +1,11 @@
+#ifndef hitting_h
+#define hitting_h
+
+class hitting {
+	int statsWon, statsLost, decision, gamesStarted, gamesWon, gamesLost;
+	public:
+		hitting();
+		void calculate();
+};
+
+#endif
