0b572ffe6c40d9bacf21b646bbd4ce3d08584ef9
ProMichael <ProMichael@outlook.com>
2015-01-05 04:20:51 -0800

    Create pitching.cpp

---
+++ project/pitching.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "pitching.h"
+
+pitching::pitching() {
+	std::cout << "Please enter the number of games started: ";
+	std::cin >> gamesStarted;
+	std::cout << std::endl << "Please enter the number of games won: ";
+	std::cin >> gamesWon;
+	std::cout << std::endl << "Please enter the number of games lost: ";
+	std::cin >> gamesLost;
+}
+
+void pitching::calculate() {
+	gamesStarted = gamesWon + gamesLost;
+	statsWon = gamesWon % gamesStarted;
+
+	std::cout << std::endl << "STATS: " << std::endl << "Total Games: " << gamesStarted << std::endl << "Total Games Won: " << gamesWon << std::endl << "Total Games Lost: " << gamesLost << std::endl << "Percent Won: " << std::endl << "Percent Lost?: " << std::endl;
+}
