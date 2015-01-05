0b7ffb9faf4d4ea361363589e63120f0dbbd5f1b
ProMichael <ProMichael@outlook.com>
2015-01-05 04:20:28 -0800

    Create pitching.h

---
+++ project/pitching.h
@@ -0,0 +1,11 @@
+#ifndef pitching_h
+#define pitching_h
+
+class pitching {
+	int statsWon, statsLost, decision, gamesStarted, gamesWon, gamesLost;
+	public:
+		pitching();
+		void calculate();
+};
+
+#endif
