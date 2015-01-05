0bc5167ad0a9de1b9d460baa0a1315ccffc10a13
ProMichael <ProMichael@outlook.com>
2015-01-05 03:53:36 -0800

    Create hitting.cpp

---
+++ project/hitting.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "hitting.h"
+
+hitting::hitting() {
+	std::cout << "Please enter the number of games started: ";
+	std::cin >> gamesStarted;
+	std::cout << std::endl << "Please enter the number of games won: ";
+	std::cin >> gamesWon;
+	std::cout << std::endl << "Please enter the number of games lost: ";
+	std::cin >> gamesLost;
+}
+void hitting::calculate() {
+	gamesStarted = gamesWon + gamesLost;
+	statsWon = gamesWon % gamesStarted;
+
+	std::cout << std::endl << "STATS: " << std::endl << "Total Games: " << gamesStarted << std::endl << "Total Games Won: " << gamesWon << std::endl << "Total Games Lost: " << gamesLost << std::endl << "Percent Won: " << std::endl << "Percent Lost?: " << std::endl;
+}
