06957ece87b4412d523e494336a77830df54f4bb
ProMichael <ProMichael@outlook.com>
2015-01-05 04:20:18 -0800

    Create fielding.h

---
+++ project/fielding.h
@@ -0,0 +1,11 @@
+#ifndef fielding_h
+#define fielding_h
+
+class fielding {
+	int statsWon, statsLost, decision, gamesStarted, gamesWon, gamesLost;
+	public:
+		fielding();
+		void calculate();
+};
+
+#endif
