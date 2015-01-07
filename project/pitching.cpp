866ab373421ff85057403806fc09573c2f33dfac
ProMichael <ProMichael@outlook.com>
2015-01-07 00:46:35 -0800

    Update pitching.cpp

--- project/pitching.cpp
+++ project/pitching.cpp
@@ -10,15 +10,15 @@ pitching::pitching() {
 	
 	cout << "Please enter the number of Games Started: ";
 	cin >> games_started;
-	cout << std::endl << "Please enter the number of Wins: ";
+	cout << endl << "Please enter the number of Wins: ";
 	cin >> wins;
-	cout << std::endl << "Please enter the number of losses: ";
+	cout << endl << "Please enter the number of losses: ";
 	cin >> losses;
 	
 	cout << "STATS: " << std::endl << "Games started: " << games_started;
-	cout << std::endl << "Wins: " << wins;
-	cout << std::endl << "Losses: " << losses;
-	cout << std::endl << "Decisions" << decisions;
-	cout << std::endl << "Winning Percentage" << winningpercentage;
-	cout << std::endl << "No Decisions" << no_decisions << std::endl;
+	cout << endl << "Wins: " << wins;
+	cout << endl << "Losses: " << losses;
+	cout << endl << "Decisions" << decisions;
+	cout << endl << "Winning Percentage" << winningpercentage;
+	cout << endl << "No Decisions" << no_decisions << endl;
 }
