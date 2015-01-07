c966bca57b02f7d8b58b8556401e63186111f97d
ProMichael <ProMichael@outlook.com>
2015-01-07 00:44:37 -0800

    Update pitching.cpp

--- project/pitching.cpp
+++ project/pitching.cpp
@@ -2,17 +2,23 @@
 #include "pitching.h"
 
 pitching::pitching() {
-	std::cout << "Please enter the number of games started: ";
-	std::cin >> gamesStarted;
-	std::cout << std::endl << "Please enter the number of games won: ";
-	std::cin >> gamesWon;
-	std::cout << std::endl << "Please enter the number of games lost: ";
-	std::cin >> gamesLost;
-}
-
-void pitching::calculate() {
-	gamesStarted = gamesWon + gamesLost;
-	statsWon = gamesWon % gamesStarted;
-
-	std::cout << std::endl << "STATS: " << std::endl << "Total Games: " << gamesStarted << std::endl << "Total Games Won: " << gamesWon << std::endl << "Total Games Lost: " << gamesLost << std::endl << "Percent Won: " << std::endl << "Percent Lost?: " << std::endl;
+    double games_started, wins, losses;
+	
+	double decisions = wins + losses;
+	double winningpercentage = wins / decisions;
+	double no_decisions = games_started - decisions;
+	
+	cout << "Please enter the number of Games Started: ";
+	cin >> games_started;
+	cout << std::endl << "Please enter the number of Wins: ";
+	cin >> wins;
+	cout << std::endl << "Please enter the number of losses: ";
+	cin >> losses;
+	
+	cout << "STATS: " << std::endl << "Games started: " << games_started;
+	cout << std::endl << "Wins: " << wins;
+	cout << std::endl << "Losses: " << losses;
+	cout << std::endl << "Decisions" << decisions;
+	cout << std::endl << "Winning Percentage" << winningpercentage;
+	cout << std::endl << "No Decisions" << no_decisions << std::endl;
 }
