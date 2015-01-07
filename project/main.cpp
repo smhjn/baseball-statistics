8be3e1e41690411d7651b9db91c4f095d90c8125
ProMichael <ProMichael@outlook.com>
2015-01-07 00:42:59 -0800

    Update main.cpp

--- project/main.cpp
+++ project/main.cpp
@@ -1,43 +1,37 @@
-/*   Authors: Michael Golebiewski           Victor Jaquez          Kevin Hrebenko
-     Desc: Main file for the baseball stats program*   Date: 12/23/14          */
+// names
 #include <iostream>
 #include <Windows.h>
 #include <string>
 #include "hitting.cpp"
 #include "fielding.cpp"
 #include "pitching.cpp"
-int menu() {
+
+using namespace std;
+
+int main() {
 	int choice;
-	std::cout << "Welcome to the baseball statistics calculator!" << std::endl;
-	std::cout << "1: Pitching" << std::endl;
-	std::cout << "2: Hitting" << std::endl;
-	std::cout << "3: Fielding" << std::endl;
-	std::cout << "4: Exit Program" << std::endl;
-	std::cin >> choice;
+	cout << "Welcome to the baseball statistics calculator!" << std::endl;
+	cout << "1: Pitching" << std::endl;
+	cout << "2: Hitting" << std::endl;
+	cout << "3: Fielding" << std::endl;
+	cout << "4: Exit Program" << std::endl;
+	cin >> choice;
 	switch(choice) {
 		case 1: 
 			pitching(); 
-			return menu();
+			return main();
 		break;
         	case 2: 
         		hitting(); 
-        		return menu();
+        		return main();
         	break;
 		case 3: 
 			fielding(); 
-			return menu();
+			return main();
 		break;
 		case 4:
-			std::cout << "Thank you for using baseball stats" << std::endl;
+			cout << "Thank you for using baseball stats" << std::endl;
 			return 0;
 		break;
 	}
-}
-int main() {
-	bool Program = true;
-
-	if(Program) {
-		menu();
-	}
-	return 0;
 }
